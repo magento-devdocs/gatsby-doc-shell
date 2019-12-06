4a2856db83a6318ce347a8a85cce2c585bd06197
James Calcaben <jcalcaben@users.noreply.github.com>
2019-12-05 21:20:21 -0600

    [Feature] MDX support (#11)
    * Add dependencies
    
    * Update config
    
    * Edit config files for creating pages from MDX files
    
    * Add mdx layout component and sample mdx pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,46 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const { createFilePath } = require("gatsby-source-filesystem")
+const path = require("path")
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === "Mdx") {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: "slug",
+      node,
+      value: `${ value }`,
+    })
+  }
+}
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allMdx {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query')
+  } 
+
+  const posts = result.data.allMdx.edges
+
+  posts.forEach(({ node  }, index) => {
+    createPage({
+      path: node.fields.slug,
+      context: { id: node.id  },
+      component: path.resolve(`./src/components/mdx-page.js`)
+    })
+  })
+}
